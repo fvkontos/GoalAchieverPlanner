84619062c4b53301ace6ab5e036e148a518699fe
Filippos-Vladimiros Kontos <47037927+fvkontos@users.noreply.github.com>
2026-06-24 00:59:10 +0300

    Merge pull request #1
    * creation of designsystem module
    
    * creation of designsystem module

---
+++ designsystem/src/main/java/com/example/goalachieverplanner/designsystem/components/AppButtons.kt
@@ -0,0 +1,32 @@
+package com.example.goalachieverplanner.designsystem.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.goalachieverplanner.designsystem.theme.AppTheme
+
+@Composable
+fun AppPrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled
+    ) {
+        Text(text = text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPrimaryButtonPreview() {
+    AppTheme {
+        AppPrimaryButton(text = "Click Me", onClick = {})
+    }
+}
